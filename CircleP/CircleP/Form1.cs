8c9762ab4694b7127f3412b4b93f882d26677650
chichoin <75055406+chichoin@users.noreply.github.com>
2022-08-20 23:03:27 +0700

    calcircle

--- CircleP/CircleP/Form1.cs
+++ CircleP/CircleP/Form1.cs
@@ -17,16 +17,20 @@ namespace CircleP
             }
             double Radius = 0, pi = 3.14, sum = 0;
             Radius = Convert.ToDouble(txtRadius.Text);
+            sum = pi * Radius * Radius;
+            lbRadius.Text = sum.ToString("");
         }
 
         private void btClr_Click(object sender, EventArgs e)
         {
-           
+            txtRadius.Clear();
+            lbRadius.Text = "";
+            txtRadius.Focus();
         }
 
         private void btExit_Click(object sender, EventArgs e)
         {
-           
+            Application.Exit();
         }
 
         private void Form1_Load(object sender, EventArgs e)
